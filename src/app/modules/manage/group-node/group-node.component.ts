49a9a9bb4255747c73ee507c5557f39914a37853
Dinh Le <dinhlespkt@gmail.com>
2023-11-11 12:13:25 +0700

    Add form dialog

--- src/app/modules/manage/group-node/group-node.component.ts
+++ src/app/modules/manage/group-node/group-node.component.ts
@@ -3,6 +3,8 @@ import { ToastService } from '@app/services/toast.service';
 import { NodeOperator } from 'src/app/data/schema/boho-v2/node-operator';
 import { NodeOperatorService } from 'src/app/data/service/node-operator.service';
 import { v4 } from 'uuid';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AddGroupNodeComponent } from './add-group-node/add-group-node.component';
 
 interface RowData {
   id: string;
@@ -17,6 +19,7 @@ interface RowData {
   styleUrls: ['group-node.component.scss', '../shared/table.scss'],
 })
 export class GroupNodeComponent implements OnInit {
+  modalService = inject(NgbModal);
   nodeOperatorService = inject(NodeOperatorService);
   toastService = inject(ToastService);
   data: RowData[] = [];
@@ -36,27 +39,34 @@ export class GroupNodeComponent implements OnInit {
     });
   }
 
-  add() {
-    const newNodeOperator: NodeOperator = {
-      id: v4(),
-      name: '',
-      describle: '',
-    };
-    this.nodeOperatorService
-      .create('0', newNodeOperator)
-      .subscribe((response) => {
-        if (!response.success) {
-          this.toastService.showError('Create new node operator failed');
-          return;
-        }
+  async add() {
+    try {
+      const { name } = await this.modalService.open(AddGroupNodeComponent)
+        .result;
 
-        this.data.push({
-          id: newNodeOperator.id,
-          name: newNodeOperator.name,
-          nodeCount: 0,
-          editable: true,
+      const newNodeOperator: NodeOperator = {
+        id: v4(),
+        name,
+        describle: '',
+      };
+      this.nodeOperatorService
+        .create('0', newNodeOperator)
+        .subscribe((response) => {
+          if (!response.success) {
+            this.toastService.showError('Create new node operator failed');
+            return;
+          }
+
+          this.data.push({
+            id: newNodeOperator.id,
+            name: newNodeOperator.name,
+            nodeCount: 0,
+            editable: false,
+          });
         });
-      });
+    } catch {
+      // Do nothing
+    }
   }
 
   trackById(_: any, item: RowData) {
