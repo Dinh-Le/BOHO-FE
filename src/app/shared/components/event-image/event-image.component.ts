4697b62e9f0150dd397a69e17c8c3ac638286a36
Dinh Le <dinhlespkt@gmail.com>
2024-01-05 06:49:30 +0700

    Show event on list view

--- src/app/shared/components/event-image/event-image.component.ts
+++ src/app/shared/components/event-image/event-image.component.ts
@@ -3,6 +3,8 @@ import {
   Component,
   ElementRef,
   Input,
+  OnChanges,
+  SimpleChanges,
   ViewChild,
 } from '@angular/core';
 import { ToastService } from '@app/services/toast.service';
@@ -10,10 +12,10 @@ import { EventService } from 'src/app/data/service/event.service';
 
 @Component({
   selector: 'app-event-image',
-  template: '<canvas #canvas style="width: 100%; height: 100%"></canvas>',
+  template: '<canvas #canvas></canvas>',
   styles: [':host{display: block; width: 100%; height: 100%}'],
 })
-export class EventImage implements AfterViewInit {
+export class EventImage implements AfterViewInit, OnChanges {
   @ViewChild('canvas') canvasRef!: ElementRef;
   @Input() event: any;
   @Input() type: 'full' | 'crop' = 'full';
@@ -26,7 +28,30 @@ export class EventImage implements AfterViewInit {
     private toastService: ToastService
   ) {}
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.canvasRef) {
+      this.update();
+    }
+  }
+
   ngAfterViewInit(): void {
+    const rect = this.elRef.nativeElement.getBoundingClientRect();
+    const canvas = this.canvasRef.nativeElement as HTMLCanvasElement;
+    canvas.width = rect.width;
+    canvas.height = rect.height;
+
+    this.update();
+  }
+
+  update() {
+    const canvas = this.canvasRef.nativeElement as HTMLCanvasElement;
+    const context = canvas.getContext('2d')!;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (!this.event) {
+      return;
+    }
+
     this.eventService
       .getImage(
         this.event.node_id,
@@ -38,45 +63,36 @@ export class EventImage implements AfterViewInit {
         next: (blod) => {
           this.image = new Image();
           this.image.onload = () => {
-            const width =
-              this.type === 'full'
-                ? this.image!.width
-                : this.image!.width *
-                  Math.abs(
-                    this.event.images_info[0].bounding_box.bottomrightx -
-                      this.event.images_info[0].bounding_box.topleftx
-                  );
-            const height =
-              this.type === 'full'
-                ? this.image!.height
-                : this.image!.height *
-                  Math.abs(
-                    this.event.images_info[0].bounding_box.bottomrighty -
-                      this.event.images_info[0].bounding_box.toplefty
-                  );
-            const sx =
-              this.type === 'full'
-                ? 0
-                : this.event.images_info[0].bounding_box.bottomrightx * width;
-            const sy =
-              this.type === 'full'
-                ? 0
-                : this.event.images_info[0].bounding_box.bottomrighty * height;
+            const canvas = this.canvasRef.nativeElement as HTMLCanvasElement;
+            let { bottomrightx, bottomrighty, topleftx, toplefty } =
+              this.event.images_info[0].bounding_box;
+            if (bottomrightx < topleftx) {
+              bottomrightx = this.event.images_info[0].bounding_box.topleftx;
+              topleftx = this.event.images_info[0].bounding_box.bottomrightx;
+            }
+            if (bottomrighty < toplefty) {
+              bottomrighty = this.event.images_info[0].bounding_box.toplefty;
+              toplefty = this.event.images_info[0].bounding_box.bottomrighty;
+            }
 
-            const rect = this.elRef.nativeElement.getBoundingClientRect();
+            const width =
+              this.image!.width *
+              (this.type === 'full' ? 1 : bottomrightx - topleftx);
+            const height =
+              this.image!.height *
+              (this.type === 'full' ? 1 : bottomrighty - toplefty);
+            const sx = this.type === 'full' ? 0 : topleftx * width;
+            const sy = this.type === 'full' ? 0 : toplefty * height;
+
             const scaleFactor = Math.min(
-              rect.width / width,
-              rect.height / height
+              canvas.width / width,
+              canvas.height / height
             );
 
-            const canvas = this.canvasRef.nativeElement as HTMLCanvasElement;
-            canvas.width = rect.width;
-            canvas.height = rect.height;
-
-            const context = canvas.getContext('2d')!;
-
-            const dx = (rect.width - width * scaleFactor) / 2;
-            const dy = (rect.height - height * scaleFactor) / 2;
+            const dx = Math.abs(canvas.width - width * scaleFactor) / 2;
+            const dy = Math.abs(canvas.height - height * scaleFactor) / 2;
+            const dw = width * scaleFactor;
+            const dh = height * scaleFactor;
             context.drawImage(
               this.image!,
               sx,
@@ -85,37 +101,17 @@ export class EventImage implements AfterViewInit {
               height,
               dx,
               dy,
-              width * scaleFactor,
-              height * scaleFactor
+              dw,
+              dh
             );
 
             // Render the bounding box if full image
             if (this.type === 'full') {
               context.beginPath();
-              const bx =
-                dx +
-                width *
-                  scaleFactor *
-                  this.event.images_info[0].bounding_box.bottomrightx;
-              const by =
-                dy +
-                height *
-                  scaleFactor *
-                  this.event.images_info[0].bounding_box.bottomrighty;
-              const bw =
-                Math.abs(
-                  this.event.images_info[0].bounding_box.bottomrightx -
-                    this.event.images_info[0].bounding_box.topleftx
-                ) *
-                width *
-                scaleFactor;
-              const bh =
-                Math.abs(
-                  this.event.images_info[0].bounding_box.bottomrighty -
-                    this.event.images_info[0].bounding_box.toplefty
-                ) *
-                height *
-                scaleFactor;
+              const bx = dx + this.image!.width * scaleFactor * topleftx;
+              const by = dy + this.image!.height * scaleFactor * toplefty;
+              const bw = (bottomrightx - topleftx) * width * scaleFactor;
+              const bh = (bottomrighty - toplefty) * height * scaleFactor;
               context.strokeStyle = 'red';
               context.rect(bx, by, bw, bh);
               context.stroke();
