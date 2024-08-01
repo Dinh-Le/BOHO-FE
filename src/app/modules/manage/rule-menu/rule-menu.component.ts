738de0d47a3286d02bfb8579f900bf1bf073d857
DInh Le <dinhlespkt@gmail.com>
2024-08-01 21:44:54 +0700

    Fix rule menu's navigation

--- src/app/modules/manage/rule-menu/rule-menu.component.ts
+++ src/app/modules/manage/rule-menu/rule-menu.component.ts
@@ -50,21 +50,24 @@ export class RuleMenuComponent {
       this.menuItemsSource[2].path =
         cameraType === 'PTZ' ? 'ptz-post-action' : 'post-action';
 
-      // Check if camera type is changed, navigate to the correct url
-      const expectedLevel3Menu =
-        cameraType === 'PTZ'
-          ? Level3Menu.PTZ_POST_ACTION
-          : Level3Menu.POST_ACTION;
-      if (navigationService.level3 !== expectedLevel3Menu) {
-        navigationService.level3 = expectedLevel3Menu;
-        navigationService.navigate();
-        return;
-      }
-
       const lastSegment =
         activatedRoute.snapshot.url[
           activatedRoute.snapshot.url.length - 1
         ].toString();
+      if (['ptz-post-action', 'post-action'].includes(lastSegment)) {
+        const expectedLevel3Menu =
+          cameraType === 'PTZ'
+            ? Level3Menu.PTZ_POST_ACTION
+            : Level3Menu.POST_ACTION;
+
+        // Check if camera type is changed, navigate to the correct url
+        if (navigationService.level3 !== expectedLevel3Menu) {
+          navigationService.level3 = expectedLevel3Menu;
+          navigationService.navigate();
+          return;
+        }
+      }
+
       for (const menuItem of this.menuItemsSource) {
         menuItem.selected = menuItem.path === lastSegment;
       }
